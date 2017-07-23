51cf0ac5aff1ee0e6dd666d8baa253f96a4bbf1b
jnhustin <jnhustin@gmail.com>
2017-07-23 08:59:36 -0700

    clean up rebase issue

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,19 @@
-<<<<<<< HEAD
-console.log('test');
-=======
->>>>>>> c4e9a0bab0d13d1553a4b2928d1f05255b5db293
 angular
 .module('AngularSandbox', ['ui.router'])
 .config([
   '$stateProvider'
 , '$urlRouterProvider'
-<<<<<<< HEAD
 , '$locationProvider'
 , function($stateProvider, $urlRouterProvider, $locationProvider) {
-=======
-, function($stateProvider, $urlRouterProvider, ) {
->>>>>>> c4e9a0bab0d13d1553a4b2928d1f05255b5db293
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
     .state('Home', {
       url: '/'
-<<<<<<< HEAD
     , templateUrl: 'app/home/home.html'
     , controller: 'HomeCtrl'
     })
 
     $locationProvider.html5Mode(true);
-=======
-    , templateUrl: 'app/home-page/home.html'
-    , controller: 'HomeCtrl'
-    })
 
-    // $locationProvider.html5Mode(true);
->>>>>>> c4e9a0bab0d13d1553a4b2928d1f05255b5db293
 }]);
