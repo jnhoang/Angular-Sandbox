0018c3fa35a5699720305e832eee91f3df56112b
jnhustin <jnhustin@gmail.com>
2017-09-25 21:57:03 -0700

    added post route to create user

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express   = require('express');
-var path      = require('path');
+var express     = require('express');
+var path        = require('path');
+var db          = require('./models');
+var bodyParser  = require('body-parser');
 
-var app       = express();
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Controllers */
+app.use('/api/users', require('./controllers/users'));
+
+
+/* Angular Catch */
 app.get('/*', function(req, res) {
  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+
+// Host
 app.listen(process.env.PORT || 3000); 
--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
-    fname: DataTypes.STRING,
-    lname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    password_reset_required: DataTypes.BOOLEAN,
-    security_question: DataTypes.STRING,
-    securit_answer: DataTypes.STRING,
-    security_reset_required: DataTypes.BOOLEAN,
-    user_role: DataTypes.STRING
+    fname                     : DataTypes.STRING,
+    lname                     : DataTypes.STRING,
+    email                     : DataTypes.STRING,
+    username                  : DataTypes.STRING,
+    password                  : DataTypes.STRING,
+    password_reset_required   : DataTypes.BOOLEAN,
+    security_question         : DataTypes.STRING,
+    securit_answer            : DataTypes.STRING,
+    security_reset_required   : DataTypes.BOOLEAN,
+    user_role                 : DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
