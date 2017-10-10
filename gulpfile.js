8849a199da9c24cab878062c16eb1e8a86777789
jnhustin <jnhustin@gmail.com>
2017-10-09 20:13:41 -0700

    refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,86 @@
 var gulp          = require('gulp');
+var autoprefixer  = require('gulp-autoprefixer');
 var browserSync   = require('browser-sync');
 var reload        = browserSync.reload;
 
-var reloadObj = { stream: true };
-
-gulp.task('html', function() {
-  gulp
-  .src(['index.html', 'app/components/**/*.html'])
-  .pipe(reload(reloadObj));
-});
-
-gulp.task('scripts', function() {
-  gulp
-  .src(['gulpfile.js', 'app/js/**/*.js', 'app/components/**/*.js' ,'!app/js/**/*.min.js'])
-  .pipe(reload(reloadObj));
-});
-
-/* WATCH TASKS */
-gulp.task('watch', function() {
-  var scriptWatchArr = [
-    'gulpfile.js', 
-    'app/js/**/*.js',
-    'app/components/**/*.js'
-  ];
-  gulp.watch(scriptWatchArr, ['scripts']);
-  gulp.watch('app/components/**/*.html', ['html']);
-})
-
-/* BROWSERSYNC TASKS */
-gulp.task('browser-sync', function() {
-  browserSyncObj = {
+var config = {
+  autoprefix      : {
+    browsers : ['last 2 versions'],
+    cascade  : false
+  },
+  browserSync     : {
     server: {
       baseDir: './app/'
     }
-  };
+  },
+  htmlSrc         : [
+    'index.html', 
+    'app/components/**/*.html'
+  ],
+  reload          : { 
+    stream: true 
+  },
+  scriptSrc       : [
+    'gulpfile.js', 
+    'app/js/**/*.js', 
+    'app/components/**/*.js',
+    '!app/js/**/*.min.js'
+  ],
+  watchHtml       : 'app/components/**/*.html',
+  watchScript     : [
+    'gulpfile.js', 
+    'app/js/**/*.js',
+    'app/components/**/*.js'
+  ],
+  watchStyle      : 'app/css/style.css'
+};
 
-  browserSync(browserSyncObj);
+/* HTML TASKS */
+gulp.task('htmls', function() {
+  gulp
+  .src(config.htmlSrc)
+  .pipe(reload(config.reload));
 });
 
 
-gulp.task('default', ['html', 'browser-sync', 'watch']);
+
+/* STYLE TASKS */
+gulp.task('styles', function() {
+  gulp
+  .src('app/css/style.css')
+  .pipe(autoprefixer(config.autoprefix))
+  .pipe(reload(config.reloadObj));
+});
+
+
+
+
+/* SCRIPT TASKS */
+gulp.task('scripts', function() {
+  gulp
+  .src(config.scriptSrc)
+  .pipe(reload(config.reload));
+});
+
+
+
+
+/* WATCH TASKS */
+gulp.task('watch', function() {
+  gulp.watch(config.watchScript, ['scripts']);
+  gulp.watch(config.watchStyle , ['style'  ]);
+  gulp.watch(config.watchHtml  , ['htmls'  ]);
+})
+
+
+
+
+/* BROWSERSYNC TASKS */
+gulp.task('browser-sync', function() {
+  browserSync(config.browserSync);
+});
+
+
+
+/* DEFAULT */
+gulp.task('default', ['htmls', 'browser-sync', 'styles', 'watch']);
