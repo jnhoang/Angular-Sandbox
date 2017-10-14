de29d6b1ff199490a93257caa64eb26518c43d53
jnhustin <jnhustin@gmail.com>
2017-10-13 20:44:11 -0700

    add JWT auth

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
+/* requires and global variables */
+require('dotenv').config();
 var express     = require('express');
 var path        = require('path');
 var db          = require('./models');
 var bodyParser  = require('body-parser');
 
+
+/* JSON WebToken Depedencies */
+var expressJWT  = require('express-jwt');
+var jwt         = require('jsonwebtoken');
+var secret      = process.env.JWT_SECRET;
+
+
+/* App */
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+/* CONFIG */
+var config = {
+  bodyParserOption  : { extended : false  },
+  expressJwtOption  : { secret   : secret },
+  securityOption    : {
+    path: [{ 
+      url       : '/api/users/signup',
+      methods   : ['POST']
+    }]
+  }
+}
+
+/* Set & Use Statements*/
+app.use(bodyParser.urlencoded(config.bodyParserOption))
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'app')));
 
 
 
 /* Controllers */
-app.use('/api/users', require('./controllers/users'));
+app.use(
+  '/api/users', 
+  expressJWT(config.expressJwtOption)
+  .unless(config.securityOption), 
+  require('./controllers/users')
+);
+
 
 
 /* Angular Catch */
@@ -21,5 +50,5 @@ app.get('/*', function(req, res) {
 });
 
 
-// Host
+/* Host */
 app.listen(process.env.PORT || 3000); 
